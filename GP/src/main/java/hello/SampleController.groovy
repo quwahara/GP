87bb4850d398fa41f1b289822198c4e659d89d5e
mitsuaki kuwahara <quwahara@gmail.com>
2015-06-09 22:10:57 +0900

    ref #268 Sprint Data JPA

--- GP/src/main/java/hello/SampleController.groovy
+++ GP/src/main/java/hello/SampleController.groovy
@@ -2,12 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// Add
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+// Add
 @ComponentScan
 @EnableAutoConfiguration
 public class SampleController {
