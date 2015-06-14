bea1d6e6acbcf3b8433add0da3b7abe77208ce4f
mitsuaki kuwahara <quwahara@gmail.com>
2015-06-14 15:26:01 +0900

    refs #268 removed verbose comments

--- GP/src/main/java/hello/SampleController.groovy
+++ GP/src/main/java/hello/SampleController.groovy
@@ -2,14 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-// Add
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-// Add
 @ComponentScan
 @EnableAutoConfiguration
 public class SampleController {
